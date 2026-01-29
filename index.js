6809f93219a9a2fc429e5898ca8b2ba45c2fa6a8
Karan Singh Manhas <karansingh33477@gmail.com>
2026-01-29 11:48:34 +0530

    commit

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const app = express()
 const PORT = 3004
 const apiRoutes = require('./server/routes/apiRoutes')
 
+
 app.use(cors());
 
 app.use(express.static(__dirname + '/server/public/'));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false, parameterLimit: 500000000 }));
 app.get('/', (req, res) => {
-    res.send('Welcome to Kiza Textiles Server')
+    res.send('Welcome to manhas server')
 })
 
 app.use('/api', apiRoutes)
